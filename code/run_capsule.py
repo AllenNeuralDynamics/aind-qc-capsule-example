d7a86a5aa4dc8dea1bb982fa6e9fe671805a5f4e
Daniel Birman <daniel.birman@alleninstitute.org>
2024-09-27 17:26:16 +0000

    Added environment.json, resources.json, postInstall; and changes in 3 other files

--- code/run_capsule.py
+++ code/run_capsule.py
@@ -1,7 +1,18 @@
 """ top level run script """
+from aind_data_schema.quality_control import QualityControl, QCEvaluation, QCMetric, Stage, Status
 
 def run():
-    """ basic run function """
-    pass
+    """ Make a QC object, dump to json """
+    metric_example = QCMetric(
+        
+    )
+
+    evaluation_example = QCEvaluation(
+
+    )
+
+    qc = QualityControl(
+        
+    )
 
 if __name__ == "__main__": run()
